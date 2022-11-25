c720b093974c9757b3386baaa138dfec2ddc8ba3
HadisMehari <hmeless@miu.edu>
2022-11-25 14:23:14 -0600

    validation added to path variable at backend

--- backend/auto-complete/src/main/java/com/bofa/autocomplete/controller/ProductController.java
+++ backend/auto-complete/src/main/java/com/bofa/autocomplete/controller/ProductController.java
@@ -3,24 +3,32 @@ package com.bofa.autocomplete.controller;
 import com.bofa.autocomplete.domain.Product;
 import com.bofa.autocomplete.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeType;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("api/product")
+@Validated
 public class ProductController {
     @Autowired
     ProductService productService;
+
+    @Value("${return.options}")
+    private int returnedOptions;
+
     @GetMapping(value ="/search/{keyword}", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<String>> search(@PathVariable String keyword){
+    public ResponseEntity<List<String>> search(@Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]{2,}$") @PathVariable  String keyword){
         try {
-            return new ResponseEntity<List<String>>(productService.search(keyword, 6), HttpStatus.OK);
+            return new ResponseEntity<List<String>>(productService.search(keyword, returnedOptions), HttpStatus.OK);
 
         } catch (Exception  ex){
             return new ResponseEntity<List<String>>(HttpStatus.BAD_REQUEST);
